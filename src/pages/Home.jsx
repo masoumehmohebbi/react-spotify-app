2908a37f1381f17aeccf227d8e778377fc3f7898
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-12-05 16:36:42 +0330

    Added PlayList CMP: Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,18 @@
 import { Footer } from "../ui/Footer";
 import Header from "../ui/Header";
 import { SideBar } from "../ui/SideBar";
+import PlayList from "./PlayList";
 
 export const Home = () => {
   return (
-    <div className="grid grid-cols-8 gap-x-2">
+    <div className="grid grid-cols-8 gap-x-2 mb-2">
       <div className="col-span-2">
         <SideBar />
       </div>
       <div className="col-span-6 flex flex-col bg-primary-800 rounded-lg">
-        <div>
-          <Header />
-        </div>
-        <div className="px-5 py-3 mb-11">
-          <h1 className="text-2xl font-bold text-secondary-0">
-            فهر‌ست‌های پخش اسپاتیفای
-          </h1>
-        </div>
-        <div>
-          <Footer />
-        </div>
+        <Header />
+        <PlayList />
+        <Footer />
       </div>
     </div>
   );
