dd296139a157cb523d456c8fdf2aec11bb769dbe
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-12-06 15:56:41 +0330

    Removed Wrapper-div TAG | App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,13 @@ import { Login } from "./pages/Login";
 import { SignUp } from "./pages/SignUp";
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route element={<AuthFormLayout />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Route>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route element={<AuthFormLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Route>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
