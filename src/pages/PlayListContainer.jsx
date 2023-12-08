e16ce9293e95cc246c867e82d4c09c1e771de09c
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-12-08 11:32:44 +0330

    Uppercase feature-PlayList

--- src/pages/PlayListContainer.jsx
+++ src/pages/PlayListContainer.jsx
@@ -1,4 +1,4 @@
-import { PlayListSection } from "./../features/playList/PlayListSection";
+import { PlayListSection } from "./../features/PlayList/PlayListSection";
 
 const PlayListContainer = () => {
   return <PlayListSection />;
