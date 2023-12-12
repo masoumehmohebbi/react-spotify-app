5eec1cb54d97f6abc05b221d5341f98d2f3d7690
masoumeMohebbi <masoumeh.mohebbi@gmail.com>
2023-12-12 20:08:49 +0330

    resolved conflict

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -4,14 +4,9 @@ import Users from "../features/admin/Users";
 const AdminDashboard = () => {
   return (
     <>
-<<<<<<< HEAD
       <DashboardHeader />
       <div className="text-secondary-0 w-full p-5 h-screen">
         <h2>main items here</h2>
-
-=======
-      <div className="text-secondary-0 w-full px-5">
->>>>>>> a6e28f43cb29c2172b08e2a631470330597d6f42
         <Users />
       </div>
     </>
